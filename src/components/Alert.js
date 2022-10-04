f95fbb7ff5473d1121c931420dae65c487a2496c
Somnath Dubey <93315832+somnath229@users.noreply.github.com>
2022-10-04 12:08:01 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,11 +6,10 @@ function alert(props) {
         return lower.charAt(0).toUpperCase() + lower.slice(1);
     }
     return (
-        props.alert && <div>
-            <div className={`alert alert-${props.alert.type} alert-dismissible fade show m-2 w-50`} role="alert">
+        <div style = {{height: '5rem'}}>
+        {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show m-2 w-50`} role="alert">
                 <strong>{capitalize(props.alert.type)}</strong>! {props.alert.msg}
-                {/* <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>  //cross button*/}
-            </div>
+            </div>}
         </div>
     )
 }
